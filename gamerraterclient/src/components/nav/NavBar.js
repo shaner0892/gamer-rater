6d40acb5efecf72fd930b68269b78b18f6276ab0
Hilary Shaner <hilaryshaner@gmail.com>
2022-05-10 14:37:49 -0500

    added nav and auth

---
+++ gamerraterclient/src/components/nav/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const history = useHistory()
+    return (
+        <ul className="navbar">
+            <Link to="/games" className="navbar__item">
+                Nav Link
+            </Link>
+            <Link to="/events" className="navbar__item">
+                Nav Link
+            </Link>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
